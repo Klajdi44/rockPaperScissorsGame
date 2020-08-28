a371ff859779a9861c59ecf67646ebf46d4ec8b1
Klajdi  Ajdini <44365608+Klajdi44@users.noreply.github.com>
2020-08-28 02:10:23 +0200

    hacked it to make it work

--- script.js
+++ script.js
@@ -14,12 +14,24 @@ function start() {
 }
 
 function clickRock() {
-  const hands = document.querySelector('#player1').classList.add('shake');
 
-  const hands1 = document.querySelector('#player2').classList.add('shake');
+  document.querySelector('#player1').classList.remove('shake');
+  document.querySelector('#player2').classList.remove('shake');
+  void document.querySelector('#player1').offsetWidth;
+  void document.querySelector('#player2').offsetWidth;
+
+  document.querySelector('#player1').classList.add('shake');
+  document.querySelector('#player2').classList.add('shake');
+
+  document.querySelector('#player1').addEventListener('animationend', changeBg);
+
+  function changeBg() {
+    document.querySelector('#player1').setAttribute('style', 'background: url(hand_rock.png); background-position:center; background-size:cover;')
+  }
 
   player = 'rock';
-  console.log('user chose', player);
+  console.log('user chose ', player);
+
 
   rememberUserChoice();
 }
@@ -27,20 +39,44 @@ function clickRock() {
 
 
 function clickPaper() {
+  document.querySelector('#player1').classList.remove('shake');
+  document.querySelector('#player2').classList.remove('shake');
+  void document.querySelector('#player1').offsetWidth;
+  void document.querySelector('#player2').offsetWidth;
+
   const hands = document.querySelector('#player1').classList.add('shake');
   const hands1 = document.querySelector('#player2').classList.add('shake');
 
+  document.querySelector('#player1').addEventListener('animationend', changeBg);
+
+  function changeBg() {
+    document.querySelector('#player1').setAttribute('style', 'background: url(hand_paper.png); background-position:center; background-size:cover;')
+
+  }
+
   player = 'paper';
-  console.log(player);
+  console.log('user chose ', player);
   rememberUserChoice();
 
 }
 function clickScissors() {
+  document.querySelector('#player1').classList.remove('shake');
+  document.querySelector('#player2').classList.remove('shake');
+  void document.querySelector('#player1').offsetWidth;
+  void document.querySelector('#player2').offsetWidth;
+
   const hands = document.querySelector('#player1').classList.add('shake');
   const hands1 = document.querySelector('#player2').classList.add('shake');
 
+  document.querySelector('#player1').addEventListener('animationend', changeBg);
+
+  function changeBg() {
+    document.querySelector('#player1').setAttribute('style', 'background: url(hand_scissors.png); background-position:center; background-size:cover;')
+
+  }
+
   player = 'scissors';
-  console.log(player);
+  console.log('user chose ', player);
   rememberUserChoice();
 }
 
@@ -54,61 +90,82 @@ function RandomComputerChoice() {
   let machineChoise = Math.floor(Math.random() * choice.length);
   console.log('computer has chosen' + choice[machineChoise]);
 
+
+  const machine = document.querySelector('#player2').addEventListener('animationend', changeBg)
+
+  function changeBg() {
+    if (choice[machineChoise] === 'paper') {
+      document.querySelector('#player2').setAttribute('style', 'background: url(hand_paper.png); background-position:center; background-size:cover;');
+    }
+    else if (choice[machineChoise] === 'rock') {
+      document.querySelector('#player2').setAttribute('style', 'background: url(hand_rock.png); background-position:center; background-size:cover;');
+    }
+    else {
+      document.querySelector('#player2').setAttribute('style', 'background: url(hand_scissors.png); background-position:center; background-size:cover;');
+    }
+
+
+
+
+
+  }
   makeDecision(choice[machineChoise]);
 }
 
 
 function makeDecision(machineChoise) {
   //TODO: make actual decision here
-  let info = document.querySelector('#info');
-  switch (true) {
-    //rock
-    case player === 'rock' && machineChoise === 'paper': return `${showLose}
-    ${ info.textContent = 'Computer chose' + " " + machineChoise
-      } ${showLose()}`;
-    case player === 'rock' && machineChoise === 'scissors': return `${showLose}
-    ${ info.textContent = 'Computer chose' + " " + machineChoise
-      } ${showWin()}`;
-    case player === 'rock' && machineChoise === 'rock': return `${showLose}
-    ${ info.textContent = 'Computer chose' + " " + machineChoise
-      } ${showTie()}`;
 
 
-    //paper
-    case player === 'paper' && machineChoise === 'scissors': return `${showLose}
-    ${ info.textContent = 'Computer chose' + " " + machineChoise
-      } ${showLose()}`;
-    case player === 'paper' && machineChoise === 'rock': return `${showLose}
-    ${ info.textContent = 'Computer chose' + " " + machineChoise
-      } ${showWin()}`;
-    case player === 'paper' && machineChoise === 'paper': return `${showLose}
-    ${ info.textContent = 'Computer chose' + " " + machineChoise
-      } ${showTie()}`;
+
+  document.querySelector('#player1').addEventListener('animationend', changeBg);
+
+  function changeBg() {
+    setTimeout(() => {
+      location.reload();
+    }, 1500)
+    let info = document.querySelector('#info');
+    switch (true) {
+      //rock
+      case player === 'rock' && machineChoise === 'paper': return `${info.textContent = 'Computer chose' + " " + machineChoise
+        } ${showLose()}`;
+
+      case player === 'rock' && machineChoise === 'scissors': return `${info.textContent = 'Computer chose' + " " + machineChoise
+        } ${showWin()}`;
+
+      case player === 'rock' && machineChoise === 'rock': return `${info.textContent = 'Computer chose' + " " + machineChoise
+        } ${showTie()}`;
 
 
-    //scissors
-    case player === 'scissors' && machineChoise === 'rock': return `${showLose}
-    ${ info.textContent = 'Computer chose' + " " + machineChoise
-      } ${showLose()}`;
-    case player === 'scissors' && machineChoise === 'paper': return `${showLose}
-    ${ info.textContent = 'Computer chose' + " " + machineChoise
-      } ${showWin()}`;
-    case player === 'scissors' && machineChoise === 'scissors': return `${showLose}
-    ${ info.textContent = 'Computer chose' + " " + machineChoise
-      } ${showTie()}`;
+      //paper
+      case player === 'paper' && machineChoise === 'scissors': return ` ${info.textContent = 'Computer chose' + " " + machineChoise
+        } ${showLose()}`;
+
+      case player === 'paper' && machineChoise === 'rock': return `${info.textContent = 'Computer chose' + " " + machineChoise
+        } ${showWin()}`;
+
+      case player === 'paper' && machineChoise === 'paper': return `${info.textContent = 'Computer chose' + " " + machineChoise
+        } ${showTie()}`;
+
+
+      //scissors
+      case player === 'scissors' && machineChoise === 'rock': return `${info.textContent = 'Computer chose' + " " + machineChoise
+        } ${showLose()}`;
+
+      case player === 'scissors' && machineChoise === 'paper': return `${info.textContent = 'Computer chose' + " " + machineChoise
+        } ${showWin()}`;
+
+      case player === 'scissors' && machineChoise === 'scissors': return `${info.textContent = 'Computer chose' + " " + machineChoise
+        } ${showTie()}`;
+
+    }
 
   }
-
-
-
-
 }
 
 function showWin() {
 
   document.querySelector('#win').classList.remove('hidden');
-
-
 }
 
 function showTie() {
